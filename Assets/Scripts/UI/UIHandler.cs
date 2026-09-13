b2af2aa27d1ff45f86dc558b83a2b53cacc4b3b1
EliteJC <83431361+EliteJC@users.noreply.github.com>
2026-09-13 19:05:16 -0400

    Moved the columns to their proper positions

--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,8 @@ public class UIHandler : MonoBehaviour
 
     [SerializeField] private Button nextWeekBtn;
     [SerializeField] private Button swapCharBtn;
+    [SerializeField] private int curCharacter;
+    [SerializeField] private GameObject[] planners; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,5 +21,21 @@ public class UIHandler : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SwapCharacters()
+    {
+        if (curCharacter == 0)
+        {
+            planners[0].gameObject.SetActive(false);
+            planners[1].gameObject.SetActive(true);
+            curCharacter = 1;
+        }
+        else
+        {
+            planners[1].gameObject.SetActive(false);
+            planners[0].gameObject.SetActive(true);
+            curCharacter = 0;
+        }
     }
 }
