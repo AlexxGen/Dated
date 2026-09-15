b8a753dcd3f1a15e2e435a45795570c5d7b202ff
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-09-15 19:22:02 -0400

    Saving

--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,8 +12,21 @@ public class UIHandler : MonoBehaviour
     [SerializeField] private Button swapCharBtn;
     [SerializeField] private GameObject[] pageFlips;
     [SerializeField] private int curCharacter;
+	private Character getCurrentCharacter()
+	{
+		switch (curCharacter)
+		{
+			case 0:
+				return Character.JESSIE;
+			case 1:
+				return Character.PETER;
+		}
+		throw new ArgumentException("Current character is neither 0 nor 1");
+	}
     [SerializeField] private GameObject[] planners; 
-
+	
+	[SerializeField] private PhoneRenderingScript phone;
+	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -40,15 +54,45 @@ public class UIHandler : MonoBehaviour
             curCharacter = 0;
         }
     }
-
+	
+	private List<Message> messagesToRenderOnPhone()
+	{
+		List<Message> toReturn = new List<Message>();
+		
+		foreach (EventCategory category in Enum.GetValues(typeof(EventCategory))) {
+			Character currentCharacter = Character.JESSIE;
+			GlobalData.GetMessage(
+				getCurrentCharacter(),
+				category,
+				gameManager.MoodOfCategory(category, getCurrentCharacter()),
+				gameManager.curWeek
+			);
+		}
+		
+		
+		
+		
+		return toReturn;
+		
+		
+		
+	}
+	
+	private void renderMessagesOnPhone()
+	{
+		phone.RenderNotifications(messagesToRenderOnPhone());
+	}
+	
     public void NextWeek()
     {
-
         pageFlips[0].gameObject.SetActive(true);
         pageFlips[1].gameObject.SetActive(true);
         pageFlips[0].GetComponent<Animator>().Play("notebook");
         pageFlips[0].GetComponent<Animator>().Play("notebook");
         StartCoroutine(waitForPageFlip());
+		
+		
+		
     }
 
     IEnumerator waitForPageFlip()
