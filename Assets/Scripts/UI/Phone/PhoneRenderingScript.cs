d07b683671e9d9fc22b34aacaab684f2f9bee7e7
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-09-14 23:04:52 -0400

    A few changes to prefab

---
+++ Assets/Scripts/UI/Phone/PhoneRenderingScript.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneRenderingScript : MonoBehaviour
+{
+	[SerializeField] private Image MessageImage;
+	[SerializeField] private TMP_Text HeaderText;
+	[SerializeField] private TMP_Text BodyText;
+	// [SerializeField] private readonly 
+	
+	
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
