591e666e104725598009ed3cc6d45c2c62e777a3
Alex Genchev <alexx.genchev@gmail.com>
2026-09-15 09:51:47 -0400

    Integrated art, set up medal materials and made title screen

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField, NotNull] private RectTransform m_root;
+    [SerializeField, NotNull] private RectTransform m_title;
+    [SerializeField, NotNull] private Button m_enterBtn;
+    [SerializeField, NotNull] private Button m_quitButton;
+
+    [Header("Position")]
+    [SerializeField, NotNull] private float m_offScreenY;
+    [SerializeField, NotNull] private float m_onScreenY;
+
+
+    private void Start()
+    {
+        m_enterBtn.onClick.AddListener(EnterGame);
+        m_quitButton.onClick.AddListener(QuitGame);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitGame();
+        }
+    }
+
+    private void EnterGame()
+    {
+        LeanTween.moveY(m_root, m_offScreenY, 0.5f).setEaseInOutSine();
+    }
+
+    private void ExitGame()
+    {
+        LeanTween.moveY(m_root, m_onScreenY, 0.5f).setEaseInOutSine();
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
