90caa757825cf4f5ab6addf47e215f86daee6b22
EliteJC <83431361+EliteJC@users.noreply.github.com>
2026-09-15 18:02:12 -0400

    Added tween on the title

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,17 +8,20 @@ public class TitleManager : MonoBehaviour
     [SerializeField, NotNull] private RectTransform m_root;
     [SerializeField, NotNull] private RectTransform m_title;
     [SerializeField, NotNull] private Button m_enterBtn;
-    [SerializeField, NotNull] private Button m_quitButton;
+    [SerializeField, NotNull] private Button m_quitBtn;
 
     [Header("Position")]
     [SerializeField, NotNull] private float m_offScreenY;
     [SerializeField, NotNull] private float m_onScreenY;
+    [SerializeField, NotNull] private bool m_onScreen = true;
 
 
     private void Start()
     {
         m_enterBtn.onClick.AddListener(EnterGame);
-        m_quitButton.onClick.AddListener(QuitGame);
+        m_quitBtn.onClick.AddListener(QuitGame);
+
+        StartCoroutine(tweenTitle());
     }
     private void Update()
     {
@@ -25,27 +29,39 @@ public class TitleManager : MonoBehaviour
         {
             ExitGame();
         }
-
-        LeanTween.scale(m_title, new Vector3(1.5f, 1.5f, 1.5f), 0.5f)
-            .setEaseInOutSine()
-            .setOnComplete(() =>
-            {
-                LeanTween.scale(m_title, new Vector3(1.5f, 1.5f, 1.5f), 0.5f).setEaseInOutSine();
-            });
     }
 
     private void EnterGame()
     {
         LeanTween.moveY(m_root, m_offScreenY, 0.5f).setEaseInOutSine();
+        m_onScreen = false;
     }
 
     private void ExitGame()
     {
         LeanTween.moveY(m_root, m_onScreenY, 0.5f).setEaseInOutSine();
+        m_onScreen = true;
+        StartCoroutine(tweenTitle());
+
     }
 
     private void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator tweenTitle()
+    {
+        while (m_onScreen)
+        {
+            LeanTween.scale(m_title, Vector3.one * 1.05f, 0.5f)
+            .setEaseInOutSine()
+            .setOnComplete(() =>
+            {
+                LeanTween.scale(m_title, Vector3.one, 0.5f).setEaseInOutSine();
+            });
+
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 }
