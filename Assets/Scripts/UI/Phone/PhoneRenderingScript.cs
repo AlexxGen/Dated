b486b5546e981648496fe3abb471a43fd6353445
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-09-14 23:30:03 -0400

    Get text to show

--- Assets/Scripts/UI/Phone/PhoneRenderingScript.cs
+++ Assets/Scripts/UI/Phone/PhoneRenderingScript.cs
@@ -1,13 +1,20 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class PhoneRenderingScript : MonoBehaviour
 {
 	[SerializeField] private Image MessageImage;
 	[SerializeField] private TMP_Text HeaderText;
 	[SerializeField] private TMP_Text BodyText;
-	// [SerializeField] private readonly 
+	
+	
+	[SerializeField] private Sprite JESSIE_TEXT_BACKGROUND;
+	[SerializeField] private Sprite PETER_TEXT_BACKGROUND;
+	[SerializeField] private Sprite DEFAULT_TEXT_BACKGROUND;
+	[SerializeField] private Sprite EMAIL_BACKGROUND;
 	
 	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,4 +28,33 @@ public class PhoneRenderingScript : MonoBehaviour
     {
         
     }
+	
+	private Sprite CorrectBackgroundImage(Message message)
+	{
+		switch (message.Medium)
+		{
+			case MessageMedium.TEXT:
+				switch (message.Sender.ToLower())
+				{
+					case "Jessiebear:)":
+						return JESSIE_TEXT_BACKGROUND;
+					case "peter <3":
+						return PETER_TEXT_BACKGROUND;
+					default:
+						return DEFAULT_TEXT_BACKGROUND;
+				}
+			case MessageMedium.EMAIL:
+				return EMAIL_BACKGROUND;
+		}
+		throw new ArgumentException("Somehow we got here");
+	}
+	
+	
+	void ShowMessage(Message message)
+	{
+		MessageImage.sprite = CorrectBackgroundImage(message);
+		HeaderText.text = message.Sender;
+		BodyText.text = message.Text;
+	}
+	
 }
