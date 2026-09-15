837600593d49151d680af4cf511c415718dff916
MrHiTech123 <78568080+MrHiTech123@users.noreply.github.com>
2026-09-15 19:05:03 -0400

    Wrong number

--- Assets/Scripts/UI/Phone/PhoneRenderingScript.cs
+++ Assets/Scripts/UI/Phone/PhoneRenderingScript.cs
@@ -31,7 +31,7 @@ public class PhoneRenderingScript : MonoBehaviour
 			}
 		});
 		
-		for (int i = 0; i < toShow.Count && i < notificationSlots.Count; ++i)
+		for (int i = 0; i < toShow.Count && i < 3; ++i)
 		{
 			notificationSlots[i].ShowMessage(toShow[i]);
 		}
